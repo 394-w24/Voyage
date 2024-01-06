e6a9f0c39d554c6d2293720e1d466ded8f9f15cb
Hongda <67817916+Hongda-OSU@users.noreply.github.com>
2024-01-06 15:08:57 -0600

    firebase config

---
+++ src/Utilities/firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA91-_18TUxVdbhFNakL1IY1awbx4U1Djw",
+  authDomain: "voyage-f348c.firebaseapp.com",
+  projectId: "voyage-f348c",
+  storageBucket: "voyage-f348c.appspot.com",
+  messagingSenderId: "264265134110",
+  appId: "1:264265134110:web:81bec94c7562488da73e0f"
+};
+
+export const getFirebaseApp = () => {
+  return !getApps().length ? initializeApp(firebaseConfig) : getApp();
+};
+
+export const getFirebaseDatabase = (firebase) => {
+  return getDatabase(firebase);
+};
+
+export const getFirebaseStorage = (firebase) => {
+  return getStorage(firebase);
+};
