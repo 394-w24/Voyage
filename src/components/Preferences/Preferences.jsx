c8a2750e75acc9bc383afcac47d5b21c17159375
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2024-01-09 20:53:53 -0600

    Feature/filter zitao (#4)
    * Add season and temperature working on filter now
    
    * Filter is working now. Working on budget
    
    * Filter is done now.
    
    * Click the card and you will go to the reference page now
    
    * 10 more locations added
    
    ---------
    
    Co-authored-by: cai.851 <cai.851@osu.edu>

--- src/components/Preferences/Preferences.jsx
+++ src/components/Preferences/Preferences.jsx
@@ -30,6 +30,14 @@ import AirportShuttleIcon from "@mui/icons-material/AirportShuttle";
 import CabinIcon from "@mui/icons-material/Cabin";
 import DirectionsBoatIcon from "@mui/icons-material/DirectionsBoat";
 
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import FilterDramaIcon from "@mui/icons-material/FilterDrama";
+import ThermostatIcon from "@mui/icons-material/Thermostat";
+import CardMedia from "@mui/material/CardMedia";
+import { useNavigate } from 'react-router-dom';
+import { ButtonBase } from '@mui/material';
+import destinationsData from '../../jsonFiles/trips.json';
 
 const drawerWidth = 240;
 
@@ -43,19 +51,102 @@ const options = [
   { icon: <DirectionsBoatIcon style={{ fontSize: 60 }} />, label: 'Cruise' }
 ];
 
+const seasons = [
+  { icon: <FilterDramaIcon style={{ fontSize: 60 }} />, label: "Spring" },
+  { icon: <WbSunnyIcon style={{ fontSize: 60 }} />, label: "Summer" },
+  { icon: <ThermostatIcon style={{ fontSize: 60 }} />, label: "Autumn" },
+  { icon: <AcUnitIcon style={{ fontSize: 60 }} />, label: "Winter" },
+];
+
+const temperatures = [
+  { icon: <AcUnitIcon style={{ fontSize: 60 }} />, label: "Cold" },
+  { icon: <ThermostatIcon style={{ fontSize: 60 }} />, label: "Mild" },
+  { icon: <WbSunnyIcon style={{ fontSize: 60 }} />, label: "Sunny" },
+];
+
 function Preferences() {
   const [checkedOptions, setCheckedOptions] = useState({});
+  const [checkedSeasons, setCheckedSeasons] = useState({});
+  const [checkedTemperatures, setCheckedTemperatures] = useState({});
+
+  const [minBudget, setMinBudget] = useState('');
+  const [maxBudget, setMaxBudget] = useState('');
+  const [hotelBudget, setHotelBudget] = useState('');
+  const [diningBudget, setDiningBudget] = useState('');
+  const [transportationBudget, setTransportationBudget] = useState('');
+  const [isRangeFilterActive, setIsRangeFilterActive] = useState(false);
+
+  const [recommendations, setRecommendations] = useState([]);
+
+  const navigate = useNavigate();
+
+  const handleCardClick = (destination) => {
+    navigate('/recommendation', { state: { destination } });
+  };
+
   const handleCheckboxChange = (label) => {
     setCheckedOptions(prev => ({
       ...prev,
       [label]: !prev[label]
     }));
   };
-  const handleGenerateRecommendations = () => {
-    const selectedOptions = Object.keys(checkedOptions).filter(key => checkedOptions[key]);
-    console.log("Selected options:", selectedOptions);
+  const handleCheckboxChangeSeason = (label) => {
+    setCheckedSeasons(prev => ({
+      ...prev,
+      [label]: !prev[label]
+    }));
+  };
+  const handleCheckboxChangeTemp = (label) => {
+    setCheckedTemperatures(prev => ({
+      ...prev,
+      [label]: !prev[label]
+    }));
   };
 
+  const handleMinBudgetChange = (event) => {
+    setMinBudget(event.target.value);
+  };
+  
+  const handleMaxBudgetChange = (event) => {
+    setMaxBudget(event.target.value);
+  };
+  
+  const handleHotelBudgetChange = (event) => {
+    setHotelBudget(event.target.value);
+  };
+  
+  const handleDiningBudgetChange = (event) => {
+    setDiningBudget(event.target.value);
+  };
+  
+  const handleTransportationBudgetChange = (event) => {
+    setTransportationBudget(event.target.value);
+  };  
+
+  const handleRangeFilterChange = (event) => {
+    setIsRangeFilterActive(event.target.checked);
+  };
+
+  const handleGenerateRecommendations = () => {
+    const selectedPreferences = Object.keys(checkedOptions).filter(key => checkedOptions[key]);
+    const selectedSeasons = Object.keys(checkedSeasons).filter(key => checkedSeasons[key]);
+    const selectedTemperatures = Object.keys(checkedTemperatures).filter(key => checkedTemperatures[key]);
+
+    const filteredDestinations = destinationsData.destinations.filter(destination => {
+      const matchesPreference = selectedPreferences.length === 0 || selectedPreferences.some(preference => destination.preferences.includes(preference));
+      const matchesSeason = selectedSeasons.length === 0 || destination.season.some(season => selectedSeasons.includes(season));
+      const matchesTemperature = selectedTemperatures.length === 0 || destination.temperature.some(temperature => selectedTemperatures.includes(temperature));
+
+      const matchesBudget = !isRangeFilterActive && (!minBudget || destination.budget.min >= minBudget) && (!maxBudget || destination.budget.max <= maxBudget);
+      const matchesHotelBudget = !hotelBudget || destination.hotelBudget === hotelBudget;
+      const matchesDiningBudget = !diningBudget || destination.diningBudget === diningBudget;
+      const matchesTransportationBudget = !transportationBudget || destination.transportationBudget === transportationBudget;
+
+      return matchesPreference && matchesSeason && matchesTemperature && matchesBudget && matchesHotelBudget && matchesDiningBudget && matchesTransportationBudget;
+    });
+
+    setRecommendations(filteredDestinations);
+  };
 
   return (
     <ThemeProvider theme={theme}>
@@ -96,6 +187,66 @@ function Preferences() {
           ))}
         </Grid>
 
+        <Typography variant="h5" gutterBottom>
+            Seasons
+          </Typography>
+          <Grid container spacing={3}>
+            {seasons.map((season, index) => (
+              <Grid item xs={12} sm={6} md={4} key={index}>
+                <Card>
+                  <CardContent>
+                    <Box
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="center"
+                    >
+                      <Box display="flex" alignItems="center">
+                        {season.icon}
+                        <Checkbox
+                        checked={checkedSeasons[season.label] || false}
+                        onChange={() => handleCheckboxChangeSeason(season.label)}
+                      />
+                      </Box>
+                      <Typography variant="subtitle1">
+                        {season.label}
+                      </Typography>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+
+          <Typography variant="h5" gutterBottom>
+            Temperatures
+          </Typography>
+          <Grid container spacing={3}>
+            {temperatures.map((temperature, index) => (
+              <Grid item xs={12} sm={6} md={4} key={index}>
+                <Card>
+                  <CardContent>
+                    <Box
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="center"
+                    >
+                      <Box display="flex" alignItems="center">
+                        {temperature.icon}
+                        <Checkbox
+                        checked={checkedTemperatures[temperature.label] || false}
+                        onChange={() => handleCheckboxChangeTemp(temperature.label)}
+                      />
+                      </Box>
+                      <Typography variant="subtitle1">
+                        {temperature.label}
+                      </Typography>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+
         <Typography variant="h5" gutterBottom sx={{ marginTop: 4 }}>
           Budget
         </Typography>
@@ -103,18 +254,22 @@ function Preferences() {
           <Grid item xs={12} sm={6} md={4}>
             <Card>
               <CardContent>
-                <Typography variant="h6">Min/Max Price Range</Typography>
+                <Typography variant="h6">Min/Max Price Range ($/day)</Typography>
                 <TextField
                   label="Min"
                   variant="outlined"
                   margin="normal"
                   fullWidth
+                  value={minBudget}
+                  onChange={handleMinBudgetChange}
                 />
                 <TextField
                   label="Max"
                   variant="outlined"
                   margin="normal"
                   fullWidth
+                  value={maxBudget}
+                  onChange={handleMaxBudgetChange}
                 />
               </CardContent>
             </Card>
@@ -123,7 +278,10 @@ function Preferences() {
             <Card>
               <CardContent>
                 <Typography variant="h6">Range Filter</Typography>
-                <Checkbox /> only show trips in this range
+                <Checkbox
+                  checked={isRangeFilterActive}
+                  onChange={handleRangeFilterChange}
+                /> only show trips in this range
               </CardContent>
             </Card>
           </Grid>
@@ -131,34 +289,56 @@ function Preferences() {
           <Grid item xs={12} md={4}>
             <Card>
               <CardContent>
-                <Typography variant="h6">Budget Details</Typography>
+                <Typography variant="h6">Budget Details ($/$$/$$$/$$$$)</Typography>
                 <TextField
                   label="Hotel"
                   variant="outlined"
                   margin="normal"
                   fullWidth
+                  value={hotelBudget}
+                  onChange={handleHotelBudgetChange}
                 />
                 <TextField
                   label="Dining"
                   variant="outlined"
                   margin="normal"
                   fullWidth
+                  value={diningBudget}
+                  onChange={handleDiningBudgetChange}
                 />
                 <TextField
                   label="Transportation"
                   variant="outlined"
                   margin="normal"
                   fullWidth
+                  value={transportationBudget}
+                  onChange={handleTransportationBudgetChange}
                 />
               </CardContent>
             </Card>
           </Grid>
         </Grid>
         <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 2 }}>
-        <Button variant="contained" size="large" color="primary" onClick={handleGenerateRecommendations}>
-          Generate Recommendations
-        </Button>
-      </Box>
+          <Button variant="contained" size="large" color="primary" onClick={handleGenerateRecommendations}>
+            Generate Recommendations
+          </Button>
+        </Box>
+      
+      <Grid container spacing={3} sx={{ marginTop: 2 }}>
+        {recommendations.map((destination, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <ButtonBase onClick={() => handleCardClick(destination)}>
+              <Card>
+                <CardContent>
+                  <Typography variant="h6">{destination.name}</Typography>
+                  <img src={destination.image} alt={destination.name} style={{ width: '100%', height: 'auto' }} />
+                </CardContent>
+              </Card>
+            </ButtonBase>
+          </Grid>
+        ))}
+      </Grid>
+
         </Container>
       </Box>
       </Box>
--- src/components/Recommendations/Recommendations.jsx
+++ src/components/Recommendations/Recommendations.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import {
   Drawer,
   Grid,
@@ -15,16 +16,29 @@ import {
   ListItemText,
   TextField,
   Container,
+  Button
 } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import theme from "/src/theme/theme.jsx";
 import CustomAppBar from "../CustomAppBar";
 import CustomDrawer from "../CustomDrawer";
 
-// Add any additional imports you might need
-
 const Recommendations = () => {
-  // Add state and methods here if needed
+  const location = useLocation();
+  const { destination } = location.state || {};
+  // const [travelPlan, setTravelPlan] = useState('');
+
+  const renderList = (items) => {
+    return items.map((item, index) => (
+      <Typography key={index} variant="body1" component="span">
+        {item}{index < items.length - 1 ? ', ' : ''}
+      </Typography>
+    ));
+  };
+
+  // const generatePlan = () => {
+  //   return setTravelPlan(items);
+  // };
 
   return (
     <ThemeProvider theme={theme}>
@@ -32,10 +46,49 @@ const Recommendations = () => {
       <CustomDrawer />
       <Container maxWidth="lg">
         <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: 8 }}>
-        <Typography variant="h5" gutterBottom>
-          Recommendations
-        </Typography>
+          <Typography variant="h5" gutterBottom>
+            Recommendations
+          </Typography>
+
+          {destination && (
+            <Card>
+              <CardContent>
+                <Typography variant="h2">{destination.name}</Typography>
+                <Typography variant="h6">Suitable Seasons: {renderList(destination.season)} ({destination.temperature})</Typography>
+                <Typography variant="h6">Travel Type: {renderList(destination.preferences)}</Typography>
+                <Typography variant="h6">Budget Estimated Range: ${destination.budget.min}/day ~ ${destination.budget.max}/day</Typography>
+                <img src={destination.image} alt={destination.name} style={{ width: '100%', height: 'auto' }} />
+              </CardContent>
+            </Card>
+          )}
+
+          <Typography variant="h5" gutterBottom sx={{ marginTop: 4 }}>
+            Mock Plan
+          </Typography>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <CardContent>
+                  <Typography variant="h6">Number of travelers</Typography>
+                  <TextField
+                    label="#"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                  />
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+          
         </Box>
+
+        <Box sx={{ display: 'flex', justifyContent: 'left', marginTop: 2, marginBottom: 5, marginLeft: 3 }}>
+          <Button variant="contained" size="large" color="primary">
+            Generate Plan
+          </Button>
+        </Box>
+        
       </Container>
     </ThemeProvider>
   );
