4a53eb81c498dfb3514959c6131346d91b5ae4d3
cai.851 <cai.851@osu.edu>
2024-01-17 22:28:35 -0600

    Profile Page is done.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useAuthState } from "../../Utilities/firebaseUtils"; // Import the useAuthState hook
 import {
   Drawer,
   Grid,
@@ -15,14 +16,15 @@ import {
   ListItemText,
   TextField,
   Container,
+  Avatar, // Import Avatar for user profile image
 } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import theme from "/src/theme/theme.jsx";
 import CustomAppBar from "../CustomAppBar";
 import CustomDrawer from "../CustomDrawer";
 
-
 const Profile = () => {
+  const [user] = useAuthState(); // Use the hook to get the current user
 
   return (
     <ThemeProvider theme={theme}>
@@ -30,9 +32,22 @@ const Profile = () => {
       <CustomDrawer /> */}
       <Container maxWidth="lg">
         <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: 8 }}>
-        <Typography variant="h5" gutterBottom>
-          Profile
-        </Typography>
+          <Typography variant="h5" gutterBottom>
+            Profile
+          </Typography>
+          {user ? (
+            <Box sx={{ marginTop: 2 }}>
+              <Avatar 
+                alt={user.displayName || "User"} 
+                src={user.photoURL} 
+                sx={{ width: 100, height: 100 }} // Adjust the size as needed
+              />
+              <Typography variant="h6">Welcome, {user.displayName || "User"}!</Typography>
+              <Typography variant="subtitle1">Email: {user.email}</Typography>
+            </Box>
+          ) : (
+            <Typography variant="subtitle1">No user is currently signed in.</Typography>
+          )}
         </Box>
       </Container>
     </ThemeProvider>
