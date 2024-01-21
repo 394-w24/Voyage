c07373c6a47098f87bd04c57212cbc51cbdbfbc2
Hongda <67817916+Hongda-OSU@users.noreply.github.com>
2024-01-21 17:42:05 -0600

    finished UI for home, sidebar, card, will do wishlist and profile later

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,38 @@
-import { useState } from "react";
 import Header from "../Header/Header";
 import Sidebar from "../Sidebar/Sidebar";
 import TravelCardItem from "../TravelCardItem/TravelCardItem";
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 import data from "../../jsonFiles/trips.json";
 import "./Home.css";
 
 const Home = () => {
-  const [destinations, setDestionations] = useState(data.destinations);
+  const location = useLocation();
+  const destinations =
+    location.state?.filteredDestinations || data.destinations;
+
+  const [addedToWishlist, setAddedToWishlist] = useState(() => {
+    const saved = localStorage.getItem("wishlist");
+    return saved ? JSON.parse(saved) : {};
+  });
+
+  useEffect(() => {
+    localStorage.setItem("wishlist", JSON.stringify(addedToWishlist));
+  }, [addedToWishlist]);
+
+  const handleAddToWishlist = (destination) => {
+    setAddedToWishlist((prevState) => {
+      const isAdded = prevState[destination.name]?.added;
+      return {
+        ...prevState,
+        [destination.name]: {
+          added: !isAdded,
+          destination: destination,
+        },
+      };
+    });
+  };
+
   return (
     <>
       <div className="home">
@@ -16,7 +42,12 @@ const Home = () => {
           <div className="travel-items-container">
             <div className="travel-items">
               {destinations.map((destination, index) => (
-                <TravelCardItem key={index} destination={destination} /> 
+                <TravelCardItem
+                  key={index}
+                  destination={destination}
+                  handleAddToWishlist={handleAddToWishlist}
+                  addedToWishlist={addedToWishlist}
+                />
               ))}
             </div>
           </div>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,36 @@
-import "./Sidebar.css"
+import { useNavigate, useLocation } from "react-router-dom";
+import { List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
+import SettingsIcon from "@mui/icons-material/Settings";
+import StarRateIcon from "@mui/icons-material/StarRate";
+import Filter from "../Filter/Filter";
+import "./Sidebar.css";
 
 const Sidebar = () => {
-    return (
-        <div className="sidebar">
-            
-        </div>
-    );
-}
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleNavigation = (path) => {
+    navigate(path);
+  };
+  return (
+    <div className="sidebar">
+      <List>
+        <ListItem button onClick={() => handleNavigation("/home")}>
+          <ListItemIcon>
+            <SettingsIcon />
+          </ListItemIcon>
+          <ListItemText primary="HOME" />
+        </ListItem>
+        <ListItem button onClick={() => handleNavigation("/recommendation")}>
+          <ListItemIcon>
+            <StarRateIcon />
+          </ListItemIcon>
+          <ListItemText primary="WISHLIST" />
+        </ListItem>
+        {location.pathname === "/home" && <Filter />}
+      </List>
+    </div>
+  );
+};
 
 export default Sidebar;
