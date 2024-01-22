75e8e2067e756cf8ab79261bb7474eb5f10e93ac
cai.851 <cai.851@osu.edu>
2024-01-22 01:56:18 -0600

    5 more cities to better filtering results

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,6 +4,7 @@ import TravelCardItem from "../TravelCardItem/TravelCardItem";
 import { useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import data from "../../jsonFiles/trips.json";
+import { Grid, Typography } from "@mui/material";
 import "./Home.css";
 
 const Home = () => {
@@ -41,14 +42,24 @@ const Home = () => {
           <Sidebar />
           <div className="travel-items-container">
             <div className="travel-items">
-              {destinations.map((destination, index) => (
-                <TravelCardItem
-                  key={index}
-                  destination={destination}
-                  handleAddToWishlist={handleAddToWishlist}
-                  addedToWishlist={addedToWishlist}
-                />
-              ))}
+              {destinations.length > 0 ? (
+                destinations.map((destination, index) => (
+                  <TravelCardItem
+                    key={index}
+                    destination={destination}
+                    handleAddToWishlist={handleAddToWishlist}
+                    addedToWishlist={addedToWishlist}
+                  />
+                ))
+              ) : (
+                <Grid item xs={12}>
+                  <div style={{ width: "500%", margin: "0 auto" }}>
+                    <Typography variant="h6" style={{ textAlign: "center" }}>
+                      No matching destinations for now, please try again.
+                    </Typography>
+                  </div>
+                </Grid>
+              )}
             </div>
           </div>
         </div>
